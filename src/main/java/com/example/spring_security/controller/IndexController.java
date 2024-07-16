3410e74190aff0b5eb47970a413f3156f0e8df6d
hyeOOO <hhyyee8800@gmail.com>
2024-07-16 16:41:44 +0900

    [feat] 환경설정 & security 초기화

---
+++ src/main/java/com/example/spring_security/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.spring_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+}
